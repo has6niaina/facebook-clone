5fad8b2cfa0fd4b9240e8f3f93c88d412b045e9b
has6niaina <tojonirinahasix02@gmail.com>
2023-10-30 16:25:47 +0300

    push all

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+//premier configuration 
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "social";
+
+// Site URL
+$site_url = "http://localhost/facebook/"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "fo"; // Your purchase code, don't give it to anyone. 
+?>
